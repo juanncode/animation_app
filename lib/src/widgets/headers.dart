04b9b572ebd1520e2f22305d20df153d3d57e2e2
juanncode <juanortizmg@gmail.com>
2022-11-05 11:38:33 -0500

    Header waves

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -45,20 +45,7 @@ class HeaderDiagonal extends StatelessWidget {
   }
 }
 
-class HeaderCurvo extends StatelessWidget {
-  const HeaderCurvo({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      child: CustomPaint(
-        painter: _HeaderCurvoPainter(),
-      ),
-    );
-  }
-}
 
 class _HeaderDiagonalPainter extends CustomPainter{
   @override
@@ -91,6 +78,21 @@ class _HeaderDiagonalPainter extends CustomPainter{
   
 }
 
+class HeaderCurvo extends StatelessWidget {
+  const HeaderCurvo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
 class _HeaderCurvoPainter extends CustomPainter{
   @override
   void paint(Canvas canvas, Size size) {
@@ -107,6 +109,51 @@ class _HeaderCurvoPainter extends CustomPainter{
     // dibujar con el path y el lapiz 
     path.lineTo(0, size.height * 0.20);
     path.quadraticBezierTo(size.width * 0.5, size.height * 0.45, size.width, size.height * 0.20);
+    path.lineTo(size.width, 0);
+    canvas.drawPath(path, paint);
+
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+  
+}
+
+
+class HeaderWaves extends StatelessWidget {
+  const HeaderWaves({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavesPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesPainter extends CustomPainter{
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // propiedades
+
+    paint.color = Colors.red;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    final path = Path();
+
+    // dibujar con el path y el lapiz 
+    path.lineTo(0, size.height * 0.30);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.45, size.width * 0.5, size.height * 0.30);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.15, size.width, size.height * 0.30);
     path.lineTo(size.width, 0);
     canvas.drawPath(path, paint);
 
